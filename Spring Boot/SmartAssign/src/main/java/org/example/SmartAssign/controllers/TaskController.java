b6b572c9aead7b94c01cdb43e28bf968791abe8a
RAHALI Aymen <aymen.rahali@soprahr.com>
2024-08-08 12:33:16 +0200

    add controller REST

--- Spring Boot/SmartAssign/src/main/java/org/example/SmartAssign/controllers/TaskController.java
+++ Spring Boot/SmartAssign/src/main/java/org/example/SmartAssign/controllers/TaskController.java
@@ -1,10 +1,12 @@
 package org.example.SmartAssign.controllers;
 
 import lombok.AllArgsConstructor;
+import org.example.SmartAssign.models.Task;
+import org.example.SmartAssign.services.ITaskService;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/Projects")
@@ -13,4 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600, allowedHeaders = "*", allowCredentials = "true")
 
 public class TaskController {
+    ITaskService taskService;
+    @GetMapping("/GetAll/Tasks")
+    public Task getTasks(@PathVariable("Task-id") String TaskID) {
+        return taskService.retrieveTask(TaskID) ;
+
+    }
+
+    @GetMapping("/GetByID/{Task-id}")
+    public List<Task> getTask() {
+        return taskService.retrieveAllTasks();
+
+    }
+
+    @PostMapping("/add-Task")
+    public Task addTask(@RequestBody Task Task) {
+        return taskService.addTask(Task);
+
+    }
+
+    @DeleteMapping("/remove-Task/{Task-id}")
+    public void removeTransports(@PathVariable("Task-id") String TaskID) {
+        taskService.removeTask(TaskID);
+    }
+
+
+
+
 }
--- Spring Boot/SmartAssign/src/main/java/org/example/SmartAssign/serviceImpls/TaskService.java
+++ Spring Boot/SmartAssign/src/main/java/org/example/SmartAssign/serviceImpls/TaskService.java
@@ -1,6 +1,41 @@
 package org.example.SmartAssign.serviceImpls;
 
+import org.example.SmartAssign.models.Task;
+import org.example.SmartAssign.repositories.TaskRepository;
 import org.example.SmartAssign.services.ITaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 
 public class TaskService implements ITaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Override
+    public List<Task> retrieveAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Task retrieveTask(String TaskID) {
+        return taskRepository.findById(TaskID).get();
+    }
+
+    @Override
+    public Task addTask(Task Task) {
+        return taskRepository.save(Task);
+    }
+
+    @Override
+    public void removeTask(String TaskID) {
+        taskRepository.deleteById(TaskID);
+
+    }
+
+    @Override
+    public Task modifyTask(Task Task) {
+        return null;
+    }
 }
